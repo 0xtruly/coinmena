c326fa7d6584be5f1a5d95125e7fb5559b928faf
dekunleroy <adebanjosegun589@gmail.com>
2021-06-14 10:23:04 +0100

    add(constants): add a constant file

---
+++ src/helper/constants.js
@@ -0,0 +1,57 @@
+const dropdownItems = [
+    { title: 'Spoken Language', dropdownText: 'Any' },
+    { title: 'Language', dropdownText: 'Any' },
+    { title: 'Date range', dropdownText: 'Any' },
+  ];
+
+  const headerNavItems = [
+    {
+        id: 'item1',
+        value: 'Explore'
+    },
+    {
+        id: 'item2',
+        value: 'Topics'
+    },
+    {
+        id: 'item3',
+        value: 'Trending'
+    },
+    {
+        id: 'item4',
+        value: 'Collections'
+    },
+    {
+        id: 'item5',
+        value: 'Events'
+    },
+    {
+        id: 'item6',
+        value: 'GitHub Sponsors'
+    },
+];
+
+const navItems = [
+    {
+      id: 'item1',
+      value: 'Pull',
+    },
+    {
+      id: 'item2',
+      value: 'Issues',
+    },
+    {
+      id: 'item3',
+      value: 'Marketplace',
+    },
+    {
+      id: 'item4',
+      value: 'Explore',
+    },
+  ];
+
+  export {
+      navItems,
+      headerNavItems,
+      dropdownItems
+  }
